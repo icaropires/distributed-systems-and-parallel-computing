68d78c3e0d9fe2bac69e91387d2835ddda1ae7ef
Ícaro Pires de Souza Aragão <icaropsa@gmail.com>
2018-03-15 20:15:32 -0300

    Add unexpected interrupt handler for tcp server
    Signed-off-by: Ícaro Pires de Souza Aragão <icaropsa@gmail.com>

--- lab_1_sockets/src/tcp_server.c
+++ lab_1_sockets/src/tcp_server.c
@@ -2,8 +2,15 @@
 #include <signal.h>
 #include "../sockets/sockets.h"
 
+void handle_interrupt(int signal);
+
+int ACCEPTED_CLIENT;
 
 int main(int argc, char *argv[]){
+	signal(SIGINT, handle_interrupt);
+	signal(SIGTERM, handle_interrupt);
+	signal(SIGABRT, handle_interrupt);
+
 	if (argc == 3){
 		int sfd = create_socket(AF_INET, SOCK_STREAM, 0);
 		struct sockaddr_in server_address = get_server_address(argv[1], argv[2]);
@@ -15,16 +22,16 @@ int main(int argc, char *argv[]){
 			struct sockaddr_in client_address;
 
 			printf("Listening to %s:%s. Waiting for connections...\n", argv[1], argv[2]);
-			int accepted_client = accept_client(sfd, (struct sockaddr_in *) &client_address, sizeof(client_address));
+			ACCEPTED_CLIENT = accept_client(sfd, (struct sockaddr_in *) &client_address, sizeof(client_address));
 
 			for(int i = 4; i >= 0; --i) {
 				Package package = {-1, -1, '#'};
-				get_message_from_connected(accepted_client, &package, sizeof(client_address), 0);
+				get_message_from_connected(ACCEPTED_CLIENT, &package, sizeof(client_address), 0);
 
 				if(package.a != -1 && package.b != -1 && package.operator != '#') {
 					package.result = calculate(package);
 
-					send_message_to_connected(accepted_client, &package, sizeof(Package), 0);
+					send_message_to_connected(ACCEPTED_CLIENT, &package, sizeof(Package), 0);
 
 					printf("Result = '%d'. Package with result sended back to client!\n"
 						   "Client %s:%u has more %d calculations\n\n",
@@ -32,7 +39,7 @@ int main(int argc, char *argv[]){
 				}
 			}
 
-			close(accepted_client);
+			close(ACCEPTED_CLIENT);
 			printf("Client %s:%u was disconected\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
 		}
 	} else {
@@ -41,4 +48,9 @@ int main(int argc, char *argv[]){
 	}
 
 	return 0;
+}
+
+void handle_interrupt(int signal){
+	fprintf(stderr, "\nClosing connection and exitting...\n");
+	close(ACCEPTED_CLIENT);
 }
