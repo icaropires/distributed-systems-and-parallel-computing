f2c6bf282cfa240711ff9ee0fa528881df62dd8f
Ícaro Pires <icaropsa@gmail.com>
2018-04-20 17:23:18 -0300

    Add django extensions to all apis and fix port for docker

--- lab_4_rest/master/master/master/settings.py
+++ lab_4_rest/master/master/master/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
--- lab_4_rest/repository/repository/repository/settings.py
+++ lab_4_rest/repository/repository/repository/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
+    'django_extensions',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
